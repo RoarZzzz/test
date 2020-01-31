c2981abbb2d52053b2f5fb0046ac9509b4d5feba
roar <610488809@qq.com>
2020-01-31 16:06:12 +0800

    1

---
+++ src/views/test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="test">
+    <p class='gg'>test</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test'
+}
+</script>
+
+<style lang="scss"  scoped>
+.test{
+   height: 30px;
+   width: 100%;
+   background-color:blue;
+    .gg{
+       color: red;
+   } 
+}
+</style>
