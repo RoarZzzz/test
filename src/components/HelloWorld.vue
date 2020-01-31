44dff35ecb4a6e926e5bc797ab89c5cc6fe4504d
roar <610488809@qq.com>
2020-01-31 15:46:29 +0800

    基础配置

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <p class='gg'>hello</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello'
+}
+</script>
+
+<style lang="scss"  scoped>
+.hello{
+   height: 30px;
+   width: 100%;
+   background-color:blue;
+    .gg{
+       color: red;
+   } 
+}
+</style>
